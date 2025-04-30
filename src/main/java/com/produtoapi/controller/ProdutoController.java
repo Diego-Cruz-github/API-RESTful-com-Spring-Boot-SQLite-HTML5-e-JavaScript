79db26862d19e3358a79de9f86d79c28bda5a316
Diego Fonte Boa <diegofonteboacruz@gmail.com>
2025-04-30 19:37:51 -0300

    Integração Client HTML / JavaScript página de listagem de produtos e página de erro.

--- src/main/java/com/produtoapi/controller/ProdutoController.java
+++ src/main/java/com/produtoapi/controller/ProdutoController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/produtos")   // http://localhost:8080/produtos
 public class ProdutoController {
